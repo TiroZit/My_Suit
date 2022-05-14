50738b61d3d0e94b3d9f2a64842f7c44097baf87
TiroZit <nikus.master@gmail.com>
2022-05-14 16:35:49 +0500

    submenu

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,6 +1,32 @@
 // Подключение функционала
-import { isMobile } from "./functions.js";
+import { isMobile } from './functions.js'
 // Подключение списка активных модулей
-import { flsModules } from "./modules.js";
+import { flsModules } from './modules.js'
 
+document.addEventListener('click', documentActions)
 
+function documentActions(e) {
+  const targetElement = e.target
+  const buttonClose =
+    targetElement.classList.contains('submenu__close') ||
+    targetElement.classList.contains('i-close')
+  if (targetElement.closest('[data-parent]') || buttonClose) {
+    const subMenuId = targetElement.dataset.parent
+      ? targetElement.dataset.parent
+      : null
+    const subMenu = document.querySelector(`[data-submenu='${subMenuId}']`)
+    const activeButton = document.querySelector('.submenu-acitve')
+    const activeSubMenu = document.querySelector('.submenu-open')
+
+    if (subMenu) {
+      if (activeButton && activeButton !== targetElement) {
+        activeButton.classList.remove('submenu-acitve')
+        activeSubMenu.classList.remove('submenu-open')
+      }
+
+      targetElement.classList.toggle('submenu-active')
+      subMenu.classList.toggle('submenu-open')
+    }
+    e.preventDefault()
+  }
+}
