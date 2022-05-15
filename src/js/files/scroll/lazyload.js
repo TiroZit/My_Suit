00861e233d6ba66d6d4b1a293e6418dce242ae9e
TiroZit <nikus.master@gmail.com>
2022-05-15 16:26:41 +0500

    welcome

--- src/js/files/scroll/lazyload.js
+++ src/js/files/scroll/lazyload.js
@@ -1,11 +1,11 @@
-import LazyLoad from "vanilla-lazyload";
+import LazyLoad from 'vanilla-lazyload'
 
 // Работает с объектами с класом ._lazy
 const lazyMedia = new LazyLoad({
-	elements_selector: '[data-src],[data-srcset]',
-	class_loaded: '_lazy-loaded',
-	use_native: true
-});
+  elements_selector: '[data-src],[data-srcset],[data-bg-multi],[bg-multi]',
+  class_loaded: '_lazy-loaded',
+  use_native: false,
+})
 
 // Обновить модуль
-// lazyMedia.update();
+// lazyMedia.update()
