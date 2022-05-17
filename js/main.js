0def11d50e5a5130791bf82d706e61694ea9b5c3
TiroZit <nikus.master@gmail.com>
2022-05-17 18:09:29 +0500

    Updates

--- js/main.js
+++ js/main.js
@@ -428,8 +428,9 @@
       document.addEventListener("click", documentActions);
       function documentActions(e) {
         const targetElement = e.target;
-        const buttonClose = targetElement.classList.contains("submenu__close") || targetElement.classList.contains("i-close");
-        if (targetElement.closest("[data-parent]") || buttonClose) {
+        const subMenuButtonClose = targetElement.classList.contains("submenu__close") || targetElement.classList.contains("i-close");
+        const searchButtonClose = targetElement.classList.contains("search__close") || targetElement.classList.contains("i-close-search");
+        if (targetElement.closest("[data-parent]") || subMenuButtonClose) {
           const subMenuId = targetElement.dataset.parent ? targetElement.dataset.parent : null;
           const subMenu = document.querySelector(`[data-submenu='${subMenuId}']`);
           const activeButton = document.querySelector(".submenu-active");
@@ -442,10 +443,16 @@
             targetElement.classList.toggle("submenu-active");
             subMenu.classList.toggle("submenu-open");
           }
-          if (buttonClose) {
+          if (subMenuButtonClose) {
             activeButton.classList.remove("submenu-active");
             activeSubMenu.classList.remove("submenu-open");
           }
+          e.preventDefault();
+        }
+        if (targetElement.closest("#searchButton") || searchButtonClose) {
+          const searchForm = document.querySelector(`.header__search`);
+          if (searchForm) searchForm.classList.toggle("search-open");
+          if (searchButtonClose) document.querySelector(`.header__search`).classList.remove("search-open");
           e.preventDefault();
         }
       }
