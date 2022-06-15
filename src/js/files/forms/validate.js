2d741e238d42ed12b5e7df91fc94529065d6e005
TiroZit <nikus.master@gmail.com>
2022-06-15 15:34:21 +0500

    fix validate

--- src/js/files/forms/validate.js
+++ src/js/files/forms/validate.js
@@ -1,26 +1,12 @@
 // Подключение модуля
 import JustValidate from 'just-validate'
 
-const validationSubscribe = new JustValidate('#form-subscribe', {
-  errorFieldCssClass: 'is-invalid',
-})
+if (document.getElementById('form-subscribe')) {
+  const validationSubscribe = new JustValidate('#form-subscribe', {
+    errorFieldCssClass: 'is-invalid',
+  })
 
-validationSubscribe.addField('#input-email', [
-  {
-    rule: 'required',
-    errorMessage: 'Обязательное поле!',
-  },
-  {
-    rule: 'email',
-    errorMessage: 'Неверный Email!',
-  },
-])
-const validationOrder = new JustValidate('#form-order', {
-  errorFieldCssClass: 'is-invalid',
-})
-
-validationOrder
-  .addField('#input-email', [
+  validationSubscribe.addField('#input-email', [
     {
       rule: 'required',
       errorMessage: 'Обязательное поле!',
@@ -30,51 +16,70 @@ validationOrder
       errorMessage: 'Неверный Email!',
     },
   ])
-  .addField('#input-first-name', [
-    {
-      rule: 'required',
-      errorMessage: 'Обязательное поле!',
-    },
-  ])
-  .addField('#input-last-name', [
-    {
-      rule: 'required',
-      errorMessage: 'Обязательное поле!',
-    },
-  ])
-  .addField('#input-phone', [
-    {
-      rule: 'required',
-      errorMessage: 'Обязательное поле!',
-    },
-  ])
-  .addField('#input-street', [
-    {
-      rule: 'required',
-      errorMessage: 'Обязательное поле!',
-    },
-  ])
-  .addField('#input-home-number', [
-    {
-      rule: 'required',
-      errorMessage: 'Обязательное поле!',
-    },
-  ])
-  .addField('#input-apartment-number', [
-    {
-      rule: 'required',
-      errorMessage: 'Обязательное поле!',
-    },
-  ])
-  .addField('#select-country', [
-    {
-      rule: 'required',
-      errorMessage: 'Обязательное поле!',
-    },
-  ])
-  .addField('#select-city', [
-    {
-      rule: 'required',
-      errorMessage: 'Обязательное поле!',
-    },
-  ])
+}
+
+if (document.getElementById('form-order')) {
+  const validationOrder = new JustValidate('#form-order', {
+    errorFieldCssClass: 'is-invalid',
+  })
+
+  validationOrder
+    .addField('#input-email', [
+      {
+        rule: 'required',
+        errorMessage: 'Обязательное поле!',
+      },
+      {
+        rule: 'email',
+        errorMessage: 'Неверный Email!',
+      },
+    ])
+    .addField('#input-first-name', [
+      {
+        rule: 'required',
+        errorMessage: 'Обязательное поле!',
+      },
+    ])
+    .addField('#input-last-name', [
+      {
+        rule: 'required',
+        errorMessage: 'Обязательное поле!',
+      },
+    ])
+    .addField('#input-phone', [
+      {
+        rule: 'required',
+        errorMessage: 'Обязательное поле!',
+      },
+    ])
+    .addField('#input-street', [
+      {
+        rule: 'required',
+        errorMessage: 'Обязательное поле!',
+      },
+    ])
+    .addField('#input-home-number', [
+      {
+        rule: 'required',
+        errorMessage: 'Обязательное поле!',
+      },
+    ])
+    .addField('#input-apartment-number', [
+      {
+        rule: 'required',
+        errorMessage: 'Обязательное поле!',
+      },
+    ])
+    .addField('#select-country', [
+      {
+        rule: 'required',
+        errorMessage: 'Обязательное поле!',
+      },
+    ])
+    .addField('#select-city', [
+      {
+        rule: 'required',
+        errorMessage: 'Обязательное поле!',
+      },
+    ])
+}
