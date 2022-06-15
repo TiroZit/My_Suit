913ce50e1ddb9692dc38b89b56cea63333bdb90f
TiroZit <nikus.master@gmail.com>
2022-06-15 14:58:24 +0500

    Валидация

--- src/js/files/forms/validate.js
+++ src/js/files/forms/validate.js
@@ -1,11 +1,11 @@
 // Подключение модуля
 import JustValidate from 'just-validate'
 
-const validation = new JustValidate('.subscribe', {
+const validationSubscribe = new JustValidate('#form-subscribe', {
   errorFieldCssClass: 'is-invalid',
 })
 
-validation.addField('#email', [
+validationSubscribe.addField('#input-email', [
   {
     rule: 'required',
     errorMessage: 'Обязательное поле!',
@@ -15,3 +15,66 @@ validation.addField('#email', [
     errorMessage: 'Неверный Email!',
   },
 ])
+const validationOrder = new JustValidate('#form-order', {
+  errorFieldCssClass: 'is-invalid',
+})
+
+validationOrder
+  .addField('#input-email', [
+    {
+      rule: 'required',
+      errorMessage: 'Обязательное поле!',
+    },
+    {
+      rule: 'email',
+      errorMessage: 'Неверный Email!',
+    },
+  ])
+  .addField('#input-first-name', [
+    {
+      rule: 'required',
+      errorMessage: 'Обязательное поле!',
+    },
+  ])
+  .addField('#input-last-name', [
+    {
+      rule: 'required',
+      errorMessage: 'Обязательное поле!',
+    },
+  ])
+  .addField('#input-phone', [
+    {
+      rule: 'required',
+      errorMessage: 'Обязательное поле!',
+    },
+  ])
+  .addField('#input-street', [
+    {
+      rule: 'required',
+      errorMessage: 'Обязательное поле!',
+    },
+  ])
+  .addField('#input-home-number', [
+    {
+      rule: 'required',
+      errorMessage: 'Обязательное поле!',
+    },
+  ])
+  .addField('#input-apartment-number', [
+    {
+      rule: 'required',
+      errorMessage: 'Обязательное поле!',
+    },
+  ])
+  .addField('#select-country', [
+    {
+      rule: 'required',
+      errorMessage: 'Обязательное поле!',
+    },
+  ])
+  .addField('#select-city', [
+    {
+      rule: 'required',
+      errorMessage: 'Обязательное поле!',
+    },
+  ])
